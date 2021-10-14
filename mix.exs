56f12a1fcc90b173d91506d6358c518b80bd930b
Jim Tubman <jtubman@shaw.ca>
2021-10-14 05:30:09 -0600

    Bumped the version number.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LRUCache.MixProject do
   def project do
     [
       app: :lru_cache,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
